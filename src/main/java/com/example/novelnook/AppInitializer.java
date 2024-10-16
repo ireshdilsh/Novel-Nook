9edc9c42af7b3ca356d6a76bc24b9c8933e68e20
ireshdilsh <ireshdilshan46@gmail.com>
2024-10-16 18:10:40 +0530

    Adding mouse event handlers to make the window draggable and remove title bar

--- src/main/java/com/example/novelnook/AppInitializer.java
+++ src/main/java/com/example/novelnook/AppInitializer.java
@@ -1,22 +1,17 @@
 package com.example.novelnook;
 
-import com.example.novelnook.db.DBConnection;
 import com.example.novelnook.utils.MailUtil;
 import com.example.novelnook.utils.WindowUtils;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.sql.Connection;
 
 public class AppInitializer extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-       new WindowUtils().setWindow("hello-view.fxml");
+       new WindowUtils().setWindow("Main-View.fxml");
     }
 
     public static void main(String[] args) throws Exception {
--- src/main/java/com/example/novelnook/utils/WindowUtils.java
+++ src/main/java/com/example/novelnook/utils/WindowUtils.java
@@ -4,16 +4,36 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import org.w3c.dom.events.MouseEvent;
 
 public class WindowUtils {
+
+    private double xOffset = 0;
+    private double yOffset = 0;
+
     public void setWindow(String fileName) {
-       try {
-           Stage stage = new Stage();
-           Parent root = FXMLLoader.load(getClass().getResource("/com/example/novelnook/"+fileName));
-           stage.setScene(new Scene(root));
-           stage.show();
-       } catch (Exception e) {
-           e.printStackTrace();
-       }
+        try {
+
+            Stage stage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("/com/example/novelnook/" + fileName));
+            // Setting stage style to undecorated
+            stage.initStyle(StageStyle.UNDECORATED);
+
+            // Adding mouse event handlers to make the window draggable
+            root.setOnMousePressed(event -> {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            });
+            root.setOnMouseDragged(event -> {
+                stage.setX(event.getScreenX() - xOffset);
+                stage.setY(event.getScreenY() - yOffset);
+            });
+            stage.setScene(new Scene(root));
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
